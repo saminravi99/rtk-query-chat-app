f8ea1c6cc84b0ee543232b8926f342dd62c62a14
saminravi99 <saminisrar1@gmail.com>
2022-09-15 22:32:24 +0600

    pessimistic cache update in add conversation

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,9 +4,13 @@ import { userLoggedOut } from "../auth/authSlice";
 const baseQuery = fetchBaseQuery({
     baseUrl: process.env.REACT_APP_API_URL,
     prepareHeaders: async (headers, { getState, endpoint }) => {
+        // console.log(endpoint);
         const token = getState()?.auth?.accessToken;
         if (token) {
             headers.set("Authorization", `Bearer ${token}`);
+        }
+        if(token && endpoint === "getMessages"){
+            headers.set("User-Email", getState()?.auth?.user?.email);    
         }
 
         return headers;
